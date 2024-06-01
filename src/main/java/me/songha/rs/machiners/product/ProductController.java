c7541389344a2aa6e2a50a60e991ced7cc8a6216
songhashin <lang_00@naver.com>
2024-06-02 02:49:02 +0900

    dependency 가져오기

--- src/main/java/me/songha/rs/machiners/product/ProductController.java
+++ src/main/java/me/songha/rs/machiners/product/ProductController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import me.songha.rs.dbconn.config.TestService;
 import me.songha.rs.machiners.error.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/product")
 public class ProductController {
     private final ProductService productService;
+    private final TestService testService;
 
     @Operation(summary = "Product 조회", description = "id로 Product를 조회한다.", operationId = "getProduct")
     @ApiResponses(value = {
@@ -26,6 +28,11 @@ public class ProductController {
     @GetMapping(value = "/id/{id}")
     public ProductDto getProduct(@PathVariable Long id) {
         return productService.getProduct(id);
+    }
+
+    @GetMapping(value = "/test")
+    public String test() {
+        return testService.test();
     }
 
 }
